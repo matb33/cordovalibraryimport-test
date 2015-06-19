372fdcecb001e2b6d43869030e37a8cbaa85783b
Mathieu Bouchard <matb33@gmail.com>
2015-06-19 11:20:46 -0400

    change to use global cordova

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 BASEPATH="$(dirname "$0")"
 BASEPATH="$(cd "$BASEPATH" && pwd)"
 
-$BASEPATH/cordova plugin rm test-plugin
+cordova plugin rm test-plugin
 rm -rf $BASEPATH/platforms/android/test-library-plugin
-$BASEPATH/cordova plugin add $BASEPATH/plugins-src/test-plugin
-$BASEPATH/cordova build android
+cordova plugin add $BASEPATH/plugins-src/test-plugin
+cordova build android
