0f230bdde8383e398294bbddd5f91443cd57d52f
Mathieu Bouchard <matb33@gmail.com>
2015-06-19 17:46:10 -0400

    make it work

--- build.sh
+++ build.sh
@@ -3,8 +3,12 @@
 BASEPATH="$(dirname "$0")"
 BASEPATH="$(cd "$BASEPATH" && pwd)"
 
-cordova plugin rm test-plugin
-rm -rf $BASEPATH/platforms/android/TabloPlayer
-rm -rf $BASEPATH/plugins/com.nuvyyo.tabloplayer
-cordova plugin add $BASEPATH/plugins-src/test-plugin
+cd $BASEPATH
+
+#cordova plugin rm test-plugin
+cordova plugin rm com.nuvyyo.tabloplayer
+#rm -rf $BASEPATH/platforms/android/TabloPlayer
+#rm -rf $BASEPATH/plugins/com.nuvyyo.tabloplayer
+#cordova plugin add $BASEPATH/plugins-src/test-plugin
+cordova plugin add $BASEPATH/plugins-src/TabloPlayer
 export ANDROID_BUILD="gradle"; cordova build android
