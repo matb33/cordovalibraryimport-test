699f85e242c4544a0222b5978e725eb18a0e05b4
Mathieu Bouchard <matb33@gmail.com>
2015-06-22 09:49:05 -0400

    just commit whatever state we are in

--- plugins-src/test-plugin/TestPlugin.java
+++ plugins-src/test-plugin/TestPlugin.java
@@ -4,7 +4,8 @@ import org.apache.cordova.CordovaPlugin;
 
 //------------------------------------------------------------------------------
 // This is ultimately what we're trying to accomplish successfully:
-import com.nuvyyo.tabloplayer.activity.PlayerActivity;
+// import com.nuvyyo.tabloplayer.activity.PlayerActivity;
+import com.google.android.libraries.cast.companionlibrary.utils.Utils;
 //------------------------------------------------------------------------------
 
 public class TestPlugin extends CordovaPlugin {}
